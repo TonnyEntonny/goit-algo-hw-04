e300332218a30f281a5bc1b39560a277204c599e
TonnyEntonny <sstonnysmirnov@gmail.com>
2026-09-12 20:33:46 +0300

    Update dz3.py

--- dz3.py
+++ dz3.py
@@ -3,18 +3,25 @@ from pathlib import Path
 from colorama import Fore, init
 
 init(autoreset=True)
+
+
+def explore_directory(path, indent=0):
+    space = " " * indent
+    for item in path.iterdir():
+        if item.is_dir():
+            print(f"{space}{Fore.BLUE}📁 Папка: {item.name}")
+            explore_directory(item, indent + 4)
+        else:
+            print(f"{space}{Fore.GREEN}📄 Файл: {item.name}")
+
+
 target_path = Path(sys.argv[1])
+
 if target_path.exists():
     print("папка існує")
     if target_path.is_dir():
-        print("цей обьект є папкою")
-        
-        for item in target_path.iterdir():
-            if item.is_dir():
-                print(f"{Fore.BLUE}📁 Папка: {item.name}")
-            else:
-                print(f"{Fore.GREEN}📄 Файл: {item.name}")
-            
+        print("цей обєкт є папкою\n")
+        explore_directory(target_path)
     else:
         print("Це файл, а не папка!")
 else:
